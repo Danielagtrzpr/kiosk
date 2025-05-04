054ff775d2969babbe38e19afa5363415d9f3b7a
Danielagtrzpr <danielagp96@gmail.com>
2025-05-04 17:01:50 -0600

    adding product details component to show the order, installing heroicons

--- components/orders/OrderSummary.tsx
+++ components/orders/OrderSummary.tsx
@@ -1,20 +1,22 @@
 "use client";
 import { useStore } from "@/src/store";
+import ProductDetails from "./ProductDetails";
 
 export default function OrderSummary() {
   const { order } = useStore();
 
   return (
-    <aside className="w-2xs bg-background p-4">
+    <aside className="w-2xs h-screen bg-background p-4 overflow-auto scrollbar-hide">
         <h2 className="font-black text-4xl">Mi Pedido</h2>
         {order.length === 0 ? (
           <p className="mt-5 text-center">No hay productos en el pedido</p>
         ) : (
           <div className="flex flex-col gap-4 mt-5">
             {order.map((product) => (
-              <div key={product.id} className="flex items-center justify-between">
-                <p className="font-black text-2xl">{product.name}</p>
-              </div>
+              <ProductDetails
+                key={product.id}
+                item={product}
+              />
             ))}
           </div>
         )}
