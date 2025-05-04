a4955715381769b5862f135d58627b1e111e88f5
Danielagtrzpr <danielagp96@gmail.com>
2025-05-04 15:32:35 -0600

    Creating an order item type based on the generated type Product and adding it to our store

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,10 @@
 import {create} from 'zustand';
+import { OrderItem } from './types';
 
 interface Store {
-
+    order: OrderItem[]
 }
 
 export const useStore = create<Store>(() => ({
-    
+    order: []
 }));
