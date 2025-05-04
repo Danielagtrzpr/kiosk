59644ab55091852c02d9bba48efa3d990996c5f7
Danielagtrzpr <danielagp96@gmail.com>
2025-05-04 15:37:08 -0600

    consuming the store in the aside component, to do this is necesary to make it a client component

--- components/orders/OrderSummary.tsx
+++ components/orders/OrderSummary.tsx
@@ -1,8 +1,23 @@
+"use client";
+import { useStore } from "@/src/store";
 
 export default function OrderSummary() {
+  const { order } = useStore();
+
   return (
     <aside className="w-2xs bg-background p-4">
         <h2 className="font-black text-4xl">Mi Pedido</h2>
+        {order.length === 0 ? (
+          <p className="mt-5 text-center">No hay productos en el pedido</p>
+        ) : (
+          <div className="flex flex-col gap-4 mt-5">
+            {order.map((product) => (
+              <div key={product.id} className="flex items-center justify-between">
+                <p className="font-black text-2xl">{product.name}</p>
+              </div>
+            ))}
+          </div>
+        )}
     </aside>
   )
 }
