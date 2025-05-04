4f825028367a199f46ca1ad78106882a6274dce2
Danielagtrzpr <danielagp96@gmail.com>
2025-05-03 20:47:38 -0600

    fetching producs by category

--- app/orders/[category]/page.tsx
+++ app/orders/[category]/page.tsx
@@ -1,9 +1,20 @@
+import {prisma} from "@/src/lib/prisma"
 
-// This is a dynamic route that will match any category
-// The category slug will be available in the params object
-// to implement this we need to create a folder with the name of the dynamic route
-export default function OrderPage({params}) {
-  console.log(params)
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category
+      }
+    }
+  })
+  return products
+}
+
+
+export default async function OrderPage({params}: {params: {category: string}}) {
+  const products = await getProducts(params.category)
+  console.log(products)
   return (
     <div>OrderPage</div>
   )
