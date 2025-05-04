04d08eaffe5f97db3783e16b99c269c5316b75eb
Danielagtrzpr <danielagp96@gmail.com>
2025-05-04 07:19:49 -0600

    params property is not available in all the components but here I can use the useParams hook to catch the param in url and compare it with the slug to be able to resalt the category selected

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,14 +1,19 @@
+"use client"
 import { Category } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
+
 
 type CategoryIconProps = {
   category: Category// This is the type of the category object returned by Prisma Prima generates types for my models
 }
 
 export default function CategoryIcon( { category }: CategoryIconProps) {
+  const params = useParams<{category:string}>()// This hook is used to get the current URL parameters
+  console.log(params)
   return (
-    <div className="flex w-full h-12 items-center p-2 gap-4 text-xl">
+    <div className={`${params.category===category.slug?"bg-amber-500":""} flex w-full h-12 items-center p-2 gap-4 text-xl`}>
         <div className="relative w-10 h-10">
             <Image fill src={`/icon_${category.slug}.svg`} alt="Image Category"/>
         </div>
