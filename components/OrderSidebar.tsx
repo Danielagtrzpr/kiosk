a851330da901a99fec93113935fd06671dec1064
Danielagtrzpr <danielagp96@gmail.com>
2025-05-03 19:02:44 -0600

    fetching data in a server component using the PrismaClient(ORM)  rather than axios

--- components/OrderSidebar.tsx
+++ components/OrderSidebar.tsx
@@ -1,6 +1,27 @@
+import { PrismaClient } from "@prisma/client"
 
-export default function OrderSidebar() {
+const prisma = new PrismaClient()
+
+//Fetching data in Nextjs is posible un 4 ways one of them is using Prisma Client(ORM) directly just in server components
+// otherwise in client components you can use axios or React Query
+async function getCategories() {
+  const categories = await prisma.category.findMany(
+  //   {
+  //   include: {
+  //     products: true,
+  //   },
+  // }
+ )
+  return categories
+}
+
+//in Nextjs in server components they can be async
+export default async function OrderSidebar() {
+  const categories = await getCategories()
+  console.log(categories)
   return (
-    <aside className="w-72 p-4">OrderSidebar</aside>
+    <aside className="w-72 p-4">
+
+    </aside>
   )
 }
