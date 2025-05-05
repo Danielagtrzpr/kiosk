85ba34e341e27452d64b691a6249d207d25eac6e
Danielagtrzpr <danielagp96@gmail.com>
2025-05-04 19:00:10 -0600

    Adding total amount to the order

--- components/orders/OrderSummary.tsx
+++ components/orders/OrderSummary.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
 
 export default function OrderSummary() {
   const { order } = useStore();
-
+  const total = useMemo(()=> order.reduce((total,item)=>total+(item.price*item.quantity),0),[order])
   return (
     <aside className="w-2xs h-screen bg-background p-4 overflow-auto scrollbar-hide">
         <h2 className="font-black text-4xl">Mi Pedido</h2>
@@ -20,6 +22,9 @@ export default function OrderSummary() {
             ))}
           </div>
         )}
+        <p className="font-bold text-xl p-4">Total a pagar: {" "}
+          <span className="font-black text-2xl">{formatCurrency(total)}</span>
+        </p>
     </aside>
   )
 }
