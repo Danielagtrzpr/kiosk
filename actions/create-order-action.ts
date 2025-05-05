f052208b58f496004ed9e6b01da629f7d2c40bf3
Danielagtrzpr <danielagp96@gmail.com>
2025-05-05 14:01:44 -0600

    creating a server action, this is new in next and in react, allows you to add info to the db as a POST but using server

---
+++ actions/create-order-action.ts
@@ -0,0 +1,6 @@
+"use server"
+
+//actions always need to be async
+export async function createOrder() {
+    console.log("handling the accion in the server from a Client component")
+}
--- components/orders/OrderSummary.tsx
+++ components/orders/OrderSummary.tsx
@@ -3,10 +3,19 @@ import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
 import { useMemo } from "react";
 import { formatCurrency } from "@/src/utils";
+import { createOrder } from "@/actions/create-order-action";
 
 export default function OrderSummary() {
   const { order } = useStore();
   const total = useMemo(()=> order.reduce((total,item)=>total+(item.price*item.quantity),0),[order])
+
+  function handleCreate(){
+    console.log("first action implementation")//this works well but
+    //with actions the thing is that "If I want to save this form info into db" this component should be a server component
+    //to achieve that I need to create a new component "use server" like this
+    createOrder()
+  }
+
   return (
     <aside className="w-2xs h-screen bg-background p-4 overflow-auto scrollbar-hide">
         <h2 className="font-black text-4xl">Mi Pedido</h2>
@@ -25,6 +34,9 @@ export default function OrderSummary() {
         <p className="font-bold text-xl p-4">Total a pagar: {" "}
           <span className="font-black text-2xl">{formatCurrency(total)}</span>
         </p>
+        <form  className="w-full mt-10 space-y-5" action={handleCreate}>
+          <input type="submit" value="Confirar Pedido" className="py-2 rounded uppercase text-white bg-indigo-700 w-full text-center cursor-pointer font-bold" />
+        </form>
     </aside>
   )
 }
