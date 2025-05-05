bbcca29f5e4b38562caf7720c6c2b44f5727c814
Danielagtrzpr <danielagp96@gmail.com>
2025-05-04 18:18:29 -0600

    avoiding duplication in the store

--- components/orders/ProductDetails.tsx
+++ components/orders/ProductDetails.tsx
@@ -1,5 +1,6 @@
 
 import { OrderItem } from "@/src/types";
+import { formatCurrency } from "@/src/utils";
 import { MinusIcon, PlusIcon, XCircleIcon } from "@heroicons/react/24/outline";
 
 type ProductDetailsProps = {
@@ -16,7 +17,7 @@ function ProductDetails({item}: ProductDetailsProps) {
             <XCircleIcon className="text-red-600 h-8 w-8" />
           </button>
         </div>
-        <p className="text-2xl text-amber-500 font-black">{item.price}</p>
+        <p className="text-2xl text-amber-500 font-black">{formatCurrency(item.price)}</p>
         <div className="flex gap-5 px-10 py-2 bg-gray-950 w-fit rounded-lg">
           <button type="button" onClick={() => {}}>
             <MinusIcon className="h-6 w-6" />
@@ -29,7 +30,7 @@ function ProductDetails({item}: ProductDetailsProps) {
           </button>
         </div>
         <p className="text-xl font-black text-gray-700">
-          Subtotal: {""}
+          Subtotal: {formatCurrency(item.price * item.quantity)}
           <span className="font-normal"></span>
         </p>
       </div>
--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,41 @@
 import {create} from 'zustand';
 import { OrderItem } from './types';
 import { Product } from '@prisma/client';
+import { it } from 'node:test';
 
 interface Store {
     order: OrderItem[],
     addToOrder: (product: Product) => void,
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set,get) => ({
     order: [],
     addToOrder: (product)=>{
         // Remove the image and categoryId from the product
+        let orderItems: OrderItem[]= [];
         const {categoryId, image,...data} = product;
 
-        // cast the product to OrderItem and adding the quantity and subtotal
-        set((state) => ({
-            order: [...state.order, {
+        if(get().order.some((item) => item.id === product.id)){
+            orderItems = get().order.map((item) => {
+                if(item.id === product.id){
+                    return {...item,
+                        quantity: item.quantity + 1, 
+                        subtotal: (item.quantity + 1) * product.price
+                    }
+                }
+                else {
+                    return item
+                }
+            })
+        } else {
+            orderItems = [...get().order, {
                 ...data, 
                 quantity: 1,
                 subtotal: 1*product.price,
-            }
-        ]}))     
+            }]
+        }
+        
+        // cast the product to OrderItem and adding the quantity and subtotal
+        set(() => ({order: orderItems}))     
     }
 }));
