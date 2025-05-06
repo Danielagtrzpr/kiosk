6e1139967d14da6bad371efd7cc2dd7bdf0027f7
Danielagtrzpr <danielagp96@gmail.com>
2025-05-06 14:38:59 -0600

    adding the order to the table: added quantity to order products, correcting a typo in schema, running migration and adding the order to the table

--- actions/create-order-action.ts
+++ actions/create-order-action.ts
@@ -1,5 +1,7 @@
 "use server"
 
+import { products } from "@/prisma/data/products"
+import { prisma } from "@/src/lib/prisma"
 import { OrderSchema } from "@/src/schema"
 
 //actions always need to be async
@@ -11,4 +13,24 @@ export async function createOrder(data:unknown) {
             errors: result.error.issues
         }
     }
+    //this "data" prop is in the server and "result.data" already pass the ZOD validation
+    console.log(result.data)
+    //so we can write in db
+    try {
+        await prisma.order.create({
+            data:{
+                name:result.data.name,
+                total:result.data.total,
+                orderProducts:{
+                    create: result.data.order.map(product=>({
+                        productId:product.id,
+                        quantity:product.quantity
+                    }))
+                }
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
