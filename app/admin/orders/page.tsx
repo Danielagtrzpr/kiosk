e18b1c761ccc87c449921a685ba05ee191677ccb
Danielagtrzpr <danielagp96@gmail.com>
2025-05-07 22:52:56 -0600

    Requesting the pending orders to the db

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,11 +1,23 @@
-import Heading from '@/components/ui/Heading'
-import React from 'react'
+import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+import React from "react";
 
-export default function OrdersPage() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+  });
+
+  return orders;
+}
+
+export default async function OrdersPage() {
+  const pendingOrders = await getPendingOrders();
+  console.log(pendingOrders);
   return (
     <div>
       <Heading>Administrar ordenes:</Heading>
-      
     </div>
-  )
+  );
 }
